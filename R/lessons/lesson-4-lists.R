c4b9b3b6cda618bd05d09382512d04af158d4a25
Simon Rapala <simonrapala833@gmail.com>
2026-09-18 09:07:45 -0400

    Learned about dataframes and vectors, the differences and their use cases. Also learned how to access indexes using various methods.

---
+++ R/lessons/lesson-4-lists.R
@@ -0,0 +1,23 @@
+#vector
+sampleNames <- c("BR5_1", "BR5_2", "BR5_3", "BR5_4")
+readCounts <- c(83000, 125000, 64000, 141000)
+
+#list
+runInfo <- list(
+  runID = "MW-2026-001",
+  userName = "Simon",
+  workflow = "Default ESV",
+  marker = "COI",
+  sampleNames = sampleNames,
+  readCounts = readCounts,
+  Classifier = "RDP COI v5.1",
+  minimumOverlap = 25,
+  completed = TRUE,
+  totalReads = sum(readCounts)
+)
+
+print(runInfo)
+class(runInfo)
+length(runInfo)
+names(runInfo)
+str(runInfo)
---
+++ R/lessons/lesson-5-dataFrames.R
@@ -0,0 +1,36 @@
+#Data Frames act as a table
+sampleNames <- c("BR5_1", "BR5_2", "BR5_3", "BR5_4")
+readCounts <- c(83000, 125000, 64000, 141000)
+passedFiltering <- c(TRUE, TRUE, FALSE, TRUE)
+
+sampleResults <- data.frame(
+  sampleName = sampleNames,
+  readCount = readCounts,
+  passedFiltering = passedFiltering
+)
+
+# print(sampleResults)
+# print(sampleResults$sampleName)
+
+# print(sampleResults[["passedFiltering"]])
+
+# print(sampleResults[2, ])
+
+# print(sampleResults[, 2])
+
+# print(sampleResults[3, 2])
+
+# print(sampleResults[c(1, 4), c("sampleName", "readCount")])
+
+# class(sampleResults["readCount"])
+
+# class(sampleResults[["readCount"]])
+
+print(sampleResults$sampleName)
+print(sampleResults[3, ])
+print(sampleResults[2,2])
+print(sampleResults[c(1, 3), ])
+print(sampleResults[c(2, 4), c("sampleName", "passedFiltering")])
+print(sampleResults["readCount"])
+print(sampleResults[["readCount"]])
+print(sampleResults[c(4,1), c("readCount", "sampleName")])
