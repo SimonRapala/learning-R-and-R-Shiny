496c49f81a9c634ea9665f383181f72fb80c93af
Simon Rapala <simonrapala833@gmail.com>
2026-09-23 10:33:28 -0400

    Reviewed how to structure and read a csv file. Also how to access different values that can be used for analysis. Further lessons will be in this same file for simplicity.

---
+++ R/lessons/lesson-20-outputs-combined.R
@@ -0,0 +1,52 @@
+mockResults <- data.frame(
+  GlobalESV = c(
+    "Zotu1", "Zotu2", "Zotu1",
+    "Zotu3", "Zotu4", "Zotu2"
+  ),
+  SampleName = c(
+    "BR5_1", "BR5_1", "BR5_2",
+    "BR5_2", "BR5_3", "BR5_3"
+  ),
+  ESVsize = c(
+    120, 45, 90, 30, 70, 65
+  ),
+  Phylum = c(
+    "Arthropoda", "Arthropoda", "Arthropoda",
+    "Annelida", "Arthropoda", "Arthropoda"
+  ),
+  Genus = c(
+    "Baetis", NA, "Baetis",
+    "Lumbricus", "Chironomus", NA
+  ),
+  Species = c(
+    "Baetis_tricaudatus", NA,
+    "Baetis_tricaudatus",
+    "Lumbricus_terrestris",
+    "Chironomus_riparius", NA
+  ),
+  sBP = c(
+    0.98, NA, 0.98,
+    0.82, 0.91, NA
+  )
+)
+
+resultsPath <- file.path(
+  getwd(),
+  "mock_results.csv"
+)
+
+write.csv(
+  mockResults,
+  resultsPath,
+  row.names = FALSE
+)
+
+# results <- read.csv(resultsPath)
+# print(results)
+# print(names(results))
+# print(nrow(results))
+# print(ncol(results))
+# print(results[1:3, ])
+# print(class(results$ESVsize))
+# print(class(results$sBP))
+# print(str(results))
