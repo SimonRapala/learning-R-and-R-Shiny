36816e945374112295769b8ce985746265d789a6
Simon Rapala <simonrapala833@gmail.com>
2026-09-24 16:23:42 -0400

    This phase is based off of test automatically based off of deterministic input. Avoids manual testing.

---
+++ R/lessons/lesson-29-basic-testing.R
@@ -0,0 +1,38 @@
+validateMode <- function(mode) {
+  mode <- tolower(mode)
+
+  if (!(mode %in% c("success", "failure"))) {
+    stop("Mode must be success or failure")
+  }
+
+  return(mode)
+}
+#Manual Testing
+# validateMode("SUCCESS")
+# validateMode("wrong")
+
+testthat::test_that("validateMode returns lowercase mode", 
+{
+  testthat::expect_equal( validateMode("SUCCESS"), "success")
+
+  testthat::expect_equal(validateMode("FAILURE"), "failure")
+})
+
+testthat::test_that("validateMode rejects invalid modes", 
+{testthat::expect_error( validateMode("wrong"), "Mode must be success or failure")
+})
+
+testthat::test_that(desc = "Testing successes with mix of lower and uppercase",
+code = {
+  testthat::expect_equal(validateMode("failuRE"), expected = "failure")
+  testthat::expect_equal(validateMode("SUCcEsS"), expected = "success")
+}
+)
+
+testthat::test_that( desc = "These should fail as they are not success or failure",
+code = {
+  testthat::expect_error(object = validateMode("cancel"), "Mode must be success or failure")
+  testthat::expect_error(object = validateMode("STOP"), "Mode must be success or failure")
+}
+
+)
